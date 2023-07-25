867836cc9ca578b19fd3462cc110d087b6b44b98
lts7602 <73794602+lts7602@users.noreply.github.com>
2023-07-25 12:39:28 -0500

    Add files via upload

---
+++ imgControl.js
@@ -0,0 +1,21 @@
+function bigImg(x) {
+    x.style.height = "20%";
+    x.style.width = "20%";
+}
+
+function normalImg(x) {
+    x.style.height = "15%";
+    x.style.width = "15%";
+}
+
+function open(){
+    window.location.href='https://www.w3docs.com';
+}
+
+function IBClick(){
+    window.location.href='https://www.ibo.org/about-the-ib/#:~:text=The%20International%20Baccalaureate%20(IB)%20is,confident%2C%20and%20caring%20young%20people.';
+}
+
+function KansasClick(){
+    window.location.href='https://www.usd259.org/site/Default.aspx?PageType=3&DomainID=4&PageID=1&ViewID=6446ee88-d30c-497e-9316-3f8874b3e108&FlexDataID=39597'
+}
